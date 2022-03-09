c5c87b9db87cc30c3d3a5b1818158e0d6bdd9e51
jay-cmd <57323437+jay-cmd@users.noreply.github.com>
2022-03-09 07:18:57 +0530

    intial files

---
+++ index.js
@@ -0,0 +1,17 @@
+// var str = "this is new"
+// console.log(str);
+// var mStr = str.split(" ");
+// console.log(typeof mStr);
+// console.log(str.split(" "));
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.trim())
+var str1 = 'Today is';
+var str2 = '      a beautiful day     '
+var str3 = ' In Hawaii.     '
+console.log(str1.concat(" ",str2.trim()," ",str3.trim()));
+var mStr = 'Mo Tu We Th Fr Sa Su'
+mStr = mStr.toUpperCase();
+console.log(mStr.split(" "))
+console.log(str1.length)
+console.log(str1.indexOf("a",2))
